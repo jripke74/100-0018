b887e6da3ee284a8ad44a540932b81a84fb6a166
Jeff Ripke <jripke74@gmail.com>
2022-02-05 11:20:49 -0800

    add handleRequest()

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require('http');
 
-const server = http.createServer();
+function handleRequest(req, res) {
+  res.statusCode = 200;
+  res.end('<h1>Hello Liberty Lake!</h1>');
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
